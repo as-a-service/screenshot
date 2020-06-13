5ff3d65baee302d12ddd4839ca8676dcd4b6a2f1
Steren Giannini <steren.giannini@gmail.com>
2020-06-13 13:45:52 -0700

    support providing width / height

--- screenshot.js
+++ screenshot.js
@@ -1,5 +1,8 @@
 const puppeteer = require('puppeteer');
 
+let browser;
+let page;
+
 /**
  * HTTP Cloud Function.
  * This function is exported by index.js, and is executed when
@@ -10,15 +13,23 @@ const puppeteer = require('puppeteer');
  */
 exports.screenshot = async (req, res) => {
     const url = req.query.url;
-  
     if (!url) {
       return res.send('Please provide URL as GET parameter, for example: <a href="?url=https://example.com">?url=https://example.com</a>');
     }
+
+    const width = req.query.width ? parseInt(req.query.width, 10) : 1280;
+    const height = req.query.height ? parseInt(req.query.height, 10) : 800;
   
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
-    });
-    const page = await browser.newPage();
+    if(!browser) {
+        browser = await puppeteer.launch({
+            args: ['--no-sandbox']
+        });
+    }
+    if(!page) {
+        page = await browser.newPage();
+    }
+
+    await page.setViewport({width, height});
     await page.goto(url);
     const imageBuffer = await page.screenshot();
     browser.close();
